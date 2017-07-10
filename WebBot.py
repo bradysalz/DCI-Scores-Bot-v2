70d6cb43dd8d230181018663359add54faeea545
brady salz <brady.salz@gmail.com>
2017-07-09 21:25:28 -0700

    refactoring web/reddit into one class

--- WebBot.py
+++ WebBot.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
+import json
 import time
+from typing import List, Dict
 
 import praw
+import requests
 
 from config import username, password, client_id, client_secret
 
@@ -11,19 +14,26 @@ BOT_INFO = (
     "[GitHub](https://github.com/bradysalz/DCI-Scores-Bot)!" +
     " Please PM me with any additional feedback.* \n\n" + "*Hope you enjoy!*")
 
+COMP_URL = 'http://bridge.competitionsuite.com/api/orgscores/GetCompetitionsByOrganization/jsonp'  # noqa
+DCI_API_ID = '96b77ec2-333e-41e9-8d7d-806a8cbe116b'
 
-class RedditBot(object):
-    """Bot that posts scores to reddit."""
 
-    def __init__(self, subreddit: str='dcicsstest'):
+class WebBot(object):
+    """Bot that interacts with the big world wide web.
+
+    In reality, only goes on two sites:
+        - dci.org, for parsing and checking for show updates
+        - reddit.com, for posting said updates
+    """
+
+    def __init__(self, show_file: str, subreddit: str='dcicsstest'):
+        self.show_file = show_file
         self.subreddit = subreddit
         self.__agent__ = 'python:dci-scores-tracker:2.0 (by /u/dynerthebard)'
         self.conn = None
 
     def connect(self):
         """Connects to reddit servers and logs in"""
-        print(client_id)
-        print(client_secret)
         self.conn = praw.Reddit(
             client_id=client_id,
             client_secret=client_secret,
@@ -45,3 +55,16 @@ class RedditBot(object):
 
         time.sleep(15)
         submission.reply(BOT_INFO)
+
+    def get_show_list(self) -> List[Dict]:
+        """Scrapes the DCI.org API site and returns a list of shows"""
+        api_keys = {'organization': DCI_API_ID, 'callback': 'jQuery'}
+
+        resp = requests.get(COMP_URL, params=api_keys)
+        content = resp.content.decode('utf-8')
+
+        # Strip off jquery tags from the response string
+        json_content = json.loads(content[7:-2])
+        competitions = json_content['competitions']
+
+        return competitions
